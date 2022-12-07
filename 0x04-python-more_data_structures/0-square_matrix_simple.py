6b1387372f0bb8cc6149e445381e7c44a63017a3
chocobae <chocobae1995@gmail.com>
2022-12-07 23:51:20 +0000

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,6 +2,6 @@
 
 def square_matrix_simple(matrix=[]):
     mat = [[i**2 for i in row] for row in matrix]
-    for row in matrix:
-       mat.append([i**2 for i in row])
+#    for row in matrix:
+#        mat.append([i**2 for i in row])
     return mat
