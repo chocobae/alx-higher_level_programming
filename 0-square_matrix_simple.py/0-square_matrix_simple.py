4282d2e74246b1be24df56f33789d6c36dedcbf7
chocobae <chocobae1995@gmail.com>
2022-12-07 22:39:07 +0000

    0-square_matrix_simple.py

--- 0-square_matrix_simple.py/0-square_matrix_simple.py
+++ 0-square_matrix_simple.py/0-square_matrix_simple.py
@@ -1,10 +1,7 @@
-quare_matrix_simple(matrix=[]):
-    """
-    wordA function that computes the square
-    value of all integers of a matrix.
-    """
-    new_matrix = []
-    for col in matrix:
-        result = list(map(lambda x: x**2, col))
-        new_matrix.append(result)
-    return new_matrix
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    mat = [[i**2 for i in row] for row in matrix]
+#    for row in matrix:
+#        mat.append([i**2 for i in row])
+    return mat
