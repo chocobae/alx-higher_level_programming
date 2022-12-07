aa5bf13cf2919f223840837ea00b80c99c0f4790
chocobae <chocobae1995@gmail.com>
2022-12-07 23:07:14 +0000

    update

--- 0-square_matrix_simple.py/1-search_replace.py
+++ 0-square_matrix_simple.py/1-search_replace.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
+
 def search_replace(my_list, search, replace):
-    """
-    A function that replaces all occurrences
-    of an element by another in a new list
-    """
-    new_list = []
-    for element in my_list:
-        if element == search:
-            new_list.append(replace)
-        else:
-            new_list.append(element)
-    return new_list
+    if my_list is None:
+        return my_list
+    new = [replace if item == search else item for item in my_list]
+    return new
