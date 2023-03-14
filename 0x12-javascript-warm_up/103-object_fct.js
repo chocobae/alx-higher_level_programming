24c5509a6bcfcb4b01ef1dab90cc7d0c25305b61
chocobae <chocobae1995@gmail.com>
2023-03-14 08:14:15 +0000

    103-object_fct.js

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-let myObject = {
+const myObject = {
   type: 'object',
   value: 12
 };
@@ -8,6 +8,7 @@ console.log(myObject);
 myObject.incr = function () {
   this.value++;
 };
+
 myObject.incr();
 console.log(myObject);
 myObject.incr();
