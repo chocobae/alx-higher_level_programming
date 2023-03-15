44ed20b542020083919cd32eae36ea0e39af147a
chocobae <chocobae1995@gmail.com>
2023-03-15 12:16:18 -0700

    103-object_fct.js

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -6,11 +6,7 @@ const myObject = {
 console.log(myObject);
 
 myObject.incr = function () {
-<<<<<<< HEAD
   myobject.value++;
-=======
-  myObject.value++;
->>>>>>> 41e3fcede37dfca2a3ccfaa9c7a51c613ff1cd67
 };
 
 myObject.incr();
