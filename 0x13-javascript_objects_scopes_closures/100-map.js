efb6c7e09610b429bc90171e6288c73996e00cda
chocobae <chocobae1995@gmail.com>
2023-03-15 11:54:56 +0000

    update

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
-const list = require('./100-data.js').list;
+const list = require('./100-data').list;
+let count = 0;
 console.log(list);
-console.log(list.map((item, index) => item * index));
+const newList = list.map(num => num * count++);
+console.log(newList);
