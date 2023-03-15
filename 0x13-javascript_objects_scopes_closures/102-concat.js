edcfcc317ad51fda4f37b0592d9871fe164ad12d
chocobae <chocobae1995@gmail.com>
2023-03-15 12:35:52 -0700

    update

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
-const args = require('process').argv;
-
-for (let i = 2; i < args.length - 1; i++) {
-  const data = fs.readFileSync(args[i], 'utf8');
-  fs.writeFileSync(args[args.length - 1], data, { flag: 'a+' });
-}
+const a = fs.readFileSync(process.argv[2], 'utf8');
+const b = fs.readFileSync(process.argv[3], 'utf8');
+fs.writeFileSync(process.argv[4], a + b);
